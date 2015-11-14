e90220afab84f48df8d908bda2f24e420778a00a
martinlosak <martinlosak2@gmail.com>
2015-11-14 23:10:01 +0100

    Simple LineChart

--- app/src/main/java/sk/fiit/shmu/MainActivity.java
+++ app/src/main/java/sk/fiit/shmu/MainActivity.java
@@ -1,15 +1,109 @@
 package sk.fiit.shmu;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private LineChart lineChart;
+    private BarChart barChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //new project
+        lineChart = (LineChart) findViewById(R.id.lineChart);
+        lineChart.setDrawGridBackground(false);
+        lineChart.setDescription("Teplota");
+        lineChart.setNoDataTextDescription("Chybajuce data.");
+        lineChart.setTouchEnabled(true);
+        lineChart.setDragEnabled(true);
+        lineChart.setScaleEnabled(true);
+        lineChart.setPinchZoom(true);
+
+        YAxis leftAxis = lineChart.getAxisLeft();
+        leftAxis.removeAllLimitLines(); // reset all limit lines to avoid overlapping lines
+        leftAxis.setAxisMaxValue(110f);
+        leftAxis.setAxisMinValue(-20f);
+        leftAxis.setStartAtZero(false);
+        leftAxis.enableGridDashedLine(10f, 10f, 0f);
+
+        // limit lines are drawn behind data (and not on top)
+        leftAxis.setDrawLimitLinesBehindData(true);
+
+        lineChart.getAxisRight().setEnabled(false);
+
+        generateDataLine(1);
+
+        lineChart.animateX(2500, Easing.EasingOption.EaseInOutQuart);
     }
+
+    private void generateDataLine(int cnt) {
+
+        ArrayList<Entry> e1 = new ArrayList<>();
+
+        for (int i = 0; i < 12; i++) {
+            e1.add(new Entry((int) (Math.random() * 65) + 40, i));
+        }
+
+        LineDataSet d1 = new LineDataSet(e1, "DataSet " + cnt);
+        d1.setLineWidth(2.5f);
+        d1.setCircleSize(5.5f);
+        d1.setHighLightColor(Color.rgb(244, 117, 117));
+        d1.setDrawValues(false);
+
+        ArrayList<Entry> e2 = new ArrayList<>();
+
+        for (int i = 0; i < 12; i++) {
+            e2.add(new Entry(e1.get(i).getVal() - 30, i));
+        }
+
+        LineDataSet d2 = new LineDataSet(e2, "DataSet " + cnt);
+        d2.setLineWidth(2.5f);
+        d2.setCircleSize(5.5f);
+        d2.setHighLightColor(Color.rgb(244, 117, 117));
+        d2.setColor(ColorTemplate.VORDIPLOM_COLORS[0]);
+        d2.setCircleColor(ColorTemplate.VORDIPLOM_COLORS[0]);
+        d2.setDrawValues(false);
+
+        ArrayList<LineDataSet> sets = new ArrayList<>();
+        sets.add(d1);
+        sets.add(d2);
+
+        LineData cd = new LineData(getMonths(), sets);
+        lineChart.setData(cd);
+    }
+
+    private ArrayList<String> getMonths() {
+
+        ArrayList<String> m = new ArrayList<>();
+        m.add("Jan");
+        m.add("Feb");
+        m.add("Mar");
+        m.add("Apr");
+        m.add("May");
+        m.add("Jun");
+        m.add("Jul");
+        m.add("Aug");
+        m.add("Sep");
+        m.add("Okt");
+        m.add("Nov");
+        m.add("Dec");
+
+        return m;
+    }
+
 }
